aeb5496a2549d2559a7b1a25b5763053095de4fc
neatab <40244313+neatab@users.noreply.github.com>
2021-07-16 08:05:28 -0700

    translations

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Translation files generated with translateci.com
+ */
+
+return [
+	"failed" => "Ces informations d’identification ne correspondent pas à nos dossiers.",
+	"throttle" => "Trop de tentatives de connexion. Veuillez réessayer en :secondes secondes.",
+];
